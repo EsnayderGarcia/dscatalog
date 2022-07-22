e100d4d3c186735d8cac02bd9926c13e2ce817de
EsnayderGarcia <scriptk.18@gmail.com>
2022-07-22 11:57:12 -0300

    Dados de Auditoria

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,8 @@
-INSERT INTO categories (name) VALUES ('Eletrônicos');
-INSERT INTO categories (name) VALUES ('Livros');
-INSERT INTO categories (name) VALUES ('Limpeza');
-INSERT INTO categories (name) VALUES ('Roupas');
-INSERT INTO categories (name) VALUES ('Higiêne');
-INSERT INTO categories (name) VALUES ('Cosméticos');
-INSERT INTO categories (name) VALUES ('Esportes');
+INSERT INTO categories (name, created_at) VALUES ('Eletrônicos', now());
+INSERT INTO categories (name, created_at) VALUES ('Livros', now());
+INSERT INTO categories (name, created_at) VALUES ('Limpeza', now());
+INSERT INTO categories (name, created_at) VALUES ('Roupas', now());
+INSERT INTO categories (name, created_at) VALUES ('Higiêne', now());
+INSERT INTO categories (name, created_at) VALUES ('Cosméticos', now());
+INSERT INTO categories (name, created_at) VALUES ('Esportes', now());
 
