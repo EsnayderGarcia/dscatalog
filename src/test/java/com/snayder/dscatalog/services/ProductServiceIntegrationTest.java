87e3a66cc2ee61bd66755033604393e351524141
EsnayderGarcia <scriptk.18@gmail.com>
2022-09-09 16:04:01 -0300

    Teste de integração ProductService método findAllPaged

--- src/test/java/com/snayder/dscatalog/services/ProductServiceIntegrationTest.java
+++ src/test/java/com/snayder/dscatalog/services/ProductServiceIntegrationTest.java
@@ -1,16 +1,22 @@
 package com.snayder.dscatalog.services;
 
+import com.snayder.dscatalog.dtos.ProductDTO;
 import com.snayder.dscatalog.repositories.ProductRepository;
 import com.snayder.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 public class ProductServiceIntegrationTest {
 
     private Long existingId;
@@ -32,7 +38,28 @@ public class ProductServiceIntegrationTest {
         totalProducts = 25L;
     }
 
+    @Test
+    public void findAllPagedShouldReturnPage() {
+        PageRequest page = PageRequest.of(0, 5);
+        Page<ProductDTO> result = productService.findAllPaged(page);
 
+        assertNotNull(result);
+
+        assertEquals(0, result.getNumber());
+        assertEquals(5, result.getSize());
+        assertEquals(totalProducts, result.getTotalElements());
+        assertEquals(ProductDTO.class, result.getContent().get(0).getClass());
+    }
+
+    @Test
+    public void findAllPagedShouldBeEmptyWhenPageDoesNotExist() {
+        int nonExistingPage = 100;
+
+        PageRequest page = PageRequest.of(nonExistingPage, 5);
+        Page<ProductDTO> result = productService.findAllPaged(page);
+
+        assertTrue(result.isEmpty());
+    }
 
     @Test
     public void deleteShouldDeleteProductWhenIdExists() {
@@ -43,7 +70,7 @@ public class ProductServiceIntegrationTest {
     @Test
     public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
         assertThrows(ResourceNotFoundException.class, () ->
-            productService.delete(nonExistingId)
+                productService.delete(nonExistingId)
         );
     }
 
